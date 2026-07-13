eb433d653b47b648d8bc0dad208a568fc5da245f
Raj6795 <rajkumar.ravindran675@gmail.com>
2026-07-13 13:31:54 +0530

    meals changes added

---
+++ app/meals/[mealSlug]/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default async function MealDetailsPage({
+  params,
+}: {
+  params: { mealSlug: string };
+}) {
+  const slug = await params;
+  const meal = await getMeal(slug.mealSlug);
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creatorEmail}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br />"),
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
